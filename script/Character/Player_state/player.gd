37a6d2068778d5f36e98a56b2b80014c8dcf6ba8
LockWood <836717992@qq.com>
2025-12-14 08:47:03 +0800

    12.14 提交了两种模式下，攻击指示出现的逻辑

--- script/Character/Player_state/player.gd
+++ script/Character/Player_state/player.gd
@@ -51,19 +51,43 @@ func _process(_delta):
 		_look_at_mouse(mouse_pos)
 	else:
 		Target_Lock_On(current_target)
+
+	if is_instance_valid(direction_Sign):
+		match player_current_aim_mode:
+			AimMode_Type.AUTO_NEAREST:
+				if is_instance_valid(current_target):
+					direction_Sign.visible = true
+					Target_Lock_On(current_target)
+				else:
+					direction_Sign.visible = false
+			AimMode_Type.MOUSE_ASSIST:
+				direction_Sign.visible = true
+				
+				if is_instance_valid(current_target):
+					Target_Lock_On(current_target)
+				else:
+					_look_at_mouse(mouse_pos)
+
+
+
+
 	
-	#寻找范围内最近的目标，并打印出来
-	var nearest_target = get_closest_target()
-	#var current_target = current_target.name if current_target else ""
-	if nearest_target and nearest_target != current_target:
-		print("找到最近的目标:",nearest_target.name)
-		current_target = nearest_target
-	elif not nearest_target and current_target:
-		print("[目标丢失] 目标 ",current_target.name,"已离开或不再是最近目标。")
-		current_target = null
+	##寻找范围内最近的目标，并打印出来
+	#var nearest_target = get_closest_target()
+	##var current_target = current_target.name if current_target else ""
+	#if nearest_target and nearest_target != current_target:
+		#print("找到最近的目标:",nearest_target.name)
+		#current_target = nearest_target
+	#elif not nearest_target and current_target:
+		#print("[目标丢失] 目标 ",current_target.name,"已离开或不再是最近目标。")
+		#current_target = null
+		#
+	
 		
-	#朝向范围内最近的对象
-	Target_Lock_On(nearest_target)
+		
+		
+	##朝向范围内最近的对象
+	#Target_Lock_On(nearest_target)
 
 #新增方法：当鼠标模式下，未锁定目标时，箭头指向鼠标
 func _look_at_mouse(mouse_position:Vector2):
